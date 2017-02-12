97233ea40d50e6ef39fe0c583f0cca7cf9b7f0b5
Danny Phillips <dannyp@procore.com>
2017-02-11 21:43:44 -0800

    render all posters to app;

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import * as photos from './national_parks';
+import * as posters from './national_parks';
 import './App.css';
 
 class App extends Component {
   render() {
-    debugger;
     return (
       <div className="App">
         <div className="App-header">
@@ -14,9 +13,9 @@ class App extends Component {
         </div>
         <p className="App-intro">
         </p>
-        <img src={photos.acadia} alt={"arches"} width={"100px"}/>
-        <img src={photos.american_samoa} alt={"arches"} width={"100px"}/>
-        <img src={photos.arches} alt={"arches"} width={"100px"}/>
+        {Object.keys(posters).map(function(park){
+          return <img src={posters[park]} alt={park} width={"100px"}/>
+        })}
       </div>
     );
   }
