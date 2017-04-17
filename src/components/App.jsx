909ff6ca1ac1d11d6458c42b311c9109abd1e295
Danny Phillips <dannyp@procore.com>
2017-04-17 13:53:29 -0700

    add stylesheet for navbar to temporarily hide it

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,7 +11,7 @@ class App extends Component {
 
     return (
       <div className="App">
-        <Navbar style={{"display": "none"}} activeItem={activeItem}/>
+        <Navbar activeItem={activeItem}/>
         {this.props.children}
       </div>
     );
