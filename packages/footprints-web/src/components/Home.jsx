69cb23f0bd93b229648dfd37cb3ff90347fe084a
Danny Phillips <dannyp@procore.com>
2017-12-02 16:09:32 -0800

    styled cards

--- packages/footprints-web/src/components/Home.jsx
+++ packages/footprints-web/src/components/Home.jsx
@@ -1,15 +1,11 @@
 import React from "react"
 import { Link } from "react-router"
-import { Card, Icon, Image } from 'semantic-ui-react'
+import { Card, Icon, Label, Image } from 'semantic-ui-react'
 import styled from 'styled-components'
 import * as posters from "../definitions/posters"
 import "./Home.css"
 
-const StyledCard = styled.Card`
-  display: inline-block;
-`;
-
-class Home extends React.Component {
+export class Home extends React.Component {
 
   render() {
     return (
@@ -18,28 +14,25 @@ class Home extends React.Component {
           <h2>Welcome to Footprints</h2>
         </div>
         {Object.keys(posters).map(function(park) {
-          return (
-            <Link to={`/parks/${park}`} key={park}>
-              <StyledCard>
+          return <Link to={`/parks/${park}`} key={park}>
+              <Card className="styled-card">
                 <Image src={posters[park]} />
-                <StyledCard.Content>
-                  <StyledCard.Header>Daniel</StyledCard.Header>
-                  <StyledCard.Meta>Joined in 2016</StyledCard.Meta>
-                  <StyledCard.Description>Daniel is a comedian living in Nashville.</StyledCard.Description>
-                </StyledCard.Content>
-                <StyledCard.Content extra>
-                  <a>
-                    <Icon name='user' />
-                    10 Friends
-                  </a>
-                </StyledCard.Content>
-              </StyledCard>
-            </Link>
-          )
+                <Card.Content>
+                  <Card.Header>Yellowstone National Park</Card.Header>
+                  <Card.Meta>Cheyenne, Wyoming</Card.Meta>
+                  <Card.Description>
+                    Daniel is a comedian living in Nashville.
+                  </Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                  <Label circular color="red">
+                    Visited
+                  </Label>
+                </Card.Content>
+              </Card>
+            </Link>;
         })}
       </div>
-    )
+    );
   }
 }
-
-export default Home
