4c28d22909bb8df8188f08a0be29870e55b54699
Danny Phillips <dannyp@procore.com>
2017-12-08 13:51:22 -0800

    add links in navbar

--- packages/footprints-web/src/components/Navbar.jsx
+++ packages/footprints-web/src/components/Navbar.jsx
@@ -12,28 +12,36 @@ export class Navbar extends Component {
 
     return <div className="Navbar">
         <Menu stackable inverted>
-          <Link to="/">
+          <Link to="/home">
             <Menu.Item onClick={this.handleItemClick}>
               <img src={nps} alt={nps} />
             </Menu.Item>
           </Link>
-          <Link to="/">
+          <Link to="/home">
             <Menu.Item name="parks" active={activeItem === "parks"}>
               Parks
             </Menu.Item>
           </Link>
-          <Menu.Menu position="right">
-            <Link to="/signIn">
-              <Menu.Item name="sign-in" active={activeItem === "sign-in"}>
-                Sign-in
-              </Menu.Item>
-            </Link>
-            <Link to="/me">
-              <Menu.Item name="my-profile" active={activeItem === "my-profile"}>
-                My Profile
-              </Menu.Item>
-            </Link>
-          </Menu.Menu>
+          <Link to="/signin">
+            <Menu.Item name="sign-in" active={activeItem === "sign-in"}>
+              Sign-In
+            </Menu.Item>
+          </Link>
+          <Link to="/signout">
+            <Menu.Item name="sign-out" active={activeItem === "sign-out"}>
+              Sign-Out
+            </Menu.Item>
+          </Link>
+          <Link to="/signup">
+            <Menu.Item name="sign-up" active={activeItem === "sign-up"}>
+              Sign-up
+            </Menu.Item>
+          </Link>
+          <Link to="/me">
+            <Menu.Item name="my-profile" active={activeItem === "my-profile"}>
+              My Profile
+            </Menu.Item>
+          </Link>
         </Menu>
       </div>;
   }
